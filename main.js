944d319bea19d8f8bae6a9279d35f7470717a680
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-05-09 11:01:08 +0200

    this should be working

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 import config from "@proxtx/config";
+import { genCombine } from "@proxtx/combine-rest/request.js";
+import { genModule } from "@proxtx/combine/combine.js";
 
 let api;
 
 export class App {
   constructor(config) {
     this.config = config;
+    this.initPromise = this.init();
+  }
+
+  async init() {
+    this.inputApi = await genCombine(this.config.input, "api", genModule);
   }
 
   async IF(condition, action) {
+    await this.initPromise;
     if (condition) {
       if (!api) api = await import("../../public/api.js");
+      for (let arg in action.arguments) {
+        action.arguments[arg] = await this.inputApi.resolveInput(
+          action.arguments[arg]
+        );
+      }
+
       return await api.execute(
         config.pwd,
         action.appName,
